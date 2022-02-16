6757f456543fccc327dbd32eecb1a2590411c40c
wangzhijian.001 <wangzhijian.001@bytedance.com>
2022-02-16 17:06:13 +0800

    初始化工程提交

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # smsbdemo
+SpringBoot2.0 demo学习
 single module SpringBoot2 demo.
+
+ENV:
+SpringBoot 2.6.3
+JDK 1.8.0
---
+++ src/test/java/com/soar/smsbdemo/SmsbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soar.smsbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsbdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
