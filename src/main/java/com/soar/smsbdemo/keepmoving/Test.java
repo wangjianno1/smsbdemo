ffec033c6e5902921ccb92965015df3a42d6eded
wangjianno1 <wangjianno1@sina.com>
2022-02-20 00:50:41 +0800

    add keepmoving package for java learning

---
+++ src/main/java/com/soar/smsbdemo/keepmoving/Test.java
@@ -0,0 +1,9 @@
+package com.soar.smsbdemo.keepmoving;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("main test running~~~");
+        long a = 1;
+        float b = 1.2;
+    }
+}
